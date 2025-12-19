985f1181b433e511720c1d68744a3bd848ce312c
simmig9393 <simmig.369@gmail.com>
2025-12-19 20:42:21 +0530

    Create Sparrow.java

---
+++ src/main/com/example/solid/lsp/good/Sparrow.java
@@ -0,0 +1,11 @@
+package com.example.solid.lsp.good;
+
+public class Sparrow implements FlyingBird {
+    public void eat() {
+        System.out.println("Sparrow eating");
+    }
+
+    public void fly() {
+        System.out.println("Sparrow flying");
+    }
+}
