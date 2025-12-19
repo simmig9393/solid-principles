4edae0ac3a4ccf8cc8bbc91a9a5e900d2f766d08
simmig9393 <simmig.369@gmail.com>
2025-12-19 20:44:44 +0530

    Create HumanWorker.java

---
+++ src/main/com/example/solid/isp/good/HumanWorker.java
@@ -0,0 +1,11 @@
+package com.example.solid.isp.good;
+
+public class HumanWorker implements Workable, Eatable {
+    public void work() {
+        System.out.println("Human working");
+    }
+
+    public void eat() {
+        System.out.println("Human eating");
+    }
+}
